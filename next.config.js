48d15c4fc18983f6fa63814fa4b86e45df882746
FabsFenris <113551554+FabsFenris@users.noreply.github.com>
2026-08-19 11:58:01 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   typescript: {
-    // Allows production builds to complete even if there are type errors
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Allows production builds to complete even if there are ESLint warnings
     ignoreDuringBuilds: true,
   },
 };
